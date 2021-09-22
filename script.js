577978fb7d14461a0ad09b82030ab0530683369d
Zachary Jayes <zacjayes@gmail.com>
2021-09-21 22:43:57 -0500

    add ripple effect to main button

--- script.js
+++ script.js
@@ -60,4 +60,25 @@ document.body.addEventListener("mousemove", (e) => {
 document.body.addEventListener("mouseleave", () => {
     mouseCircle.style.opacity = '0';
     mouseDot.style.opacity = '0';
+});
+
+//Main Button
+const mainBtn = document.querySelector('.main-btn');
+
+let ripple;
+
+mainBtn.addEventListener('mouseenter', (e) => {
+    const left = e.clientX - e.target.getBoundingClientRect().left;
+    const top = e.clientY - e.target.getBoundingClientRect().top;
+
+    ripple = document.createElement('div')
+    ripple.classList.add('ripple')
+    ripple.style.left = `${left}px`;
+    ripple.style.top = `${top}px`;
+    mainBtn.prepend(ripple)
 })
+
+mainBtn.addEventListener('mouseleave', () => {
+    mainBtn.removeChild(ripple)
+})
+//End of Main Button
