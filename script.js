3780104a0649fe6e36f53e60c193dd46037ad40b
Zachary Jayes <zacjayes@gmail.com>
2021-09-21 22:13:47 -0500

    animated circles and created and customized main button

--- script.js
+++ script.js
@@ -1,17 +1,59 @@
+//Mouse Circle 
 const mouseCircle = document.querySelector(".mouse-circle");
 const mouseDot = document.querySelector(".mouse-dot");
 
-//Mouse Circle 
 const mouseCircleFn = (x, y) => {
     mouseCircle.style.cssText = `top: ${y}px; left: ${x}px; opacity: 1`;
     mouseDot.style.cssText = `top: ${y}px; left: ${x}px; opacity: 1`;
 };
+//End of Mouse Circle
+
+//Animated Circles
+const circles = document.querySelectorAll('.circle')
+const mainImg = document.querySelector('.main-circle img');
+
+let mX = 0
+let mY = 0
+const z = 100
+
+const animateCircles = (e, x, y) => {
+
+    if (x < mX) {
+        circles.forEach(circle => {
+            circle.style.left = `${z}px`;
+        });
+        mainImg.style.left = `${z}px`;
+    } else if (x > mX) {
+        circles.forEach((circle) => {
+            circle.style.left = `-${z}px`;
+        });
+        mainImg.style.left = `-${z}px`;
+
+    }
+    if (y < mY) {
+        circles.forEach(circle => {
+            circle.style.top = `${z}px`;
+        });
+        mainImg.style.top = `${z}px`;
+    } else if (y > mY) {
+        circles.forEach((circle) => {
+            circle.style.top = `-${z}px`;
+        });
+        mainImg.style.top = `-${z}px`;
+    }
+
+    mX = e.clientX
+    mY = e.clientY
+
+}
+//End of Animated Circles
 
 document.body.addEventListener("mousemove", (e) => {
     let x = e.clientX;
     let y = e.clientY;
 
     mouseCircleFn(x, y);
+    animateCircles(e, x, y);
 });
 
 //makes mouse disappear when off screen
@@ -19,4 +61,3 @@ document.body.addEventListener("mouseleave", () => {
     mouseCircle.style.opacity = '0';
     mouseDot.style.opacity = '0';
 })
-//End of MDot
