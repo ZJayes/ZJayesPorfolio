07ea1f5a357bf5cd224076286fd4e65fd70ef9a2
Zachary Jayes <zacjayes@gmail.com>
2021-09-23 22:18:27 -0500

    customized projects when big

--- script.js
+++ script.js
@@ -103,16 +103,40 @@ Array.from(aboutMeTextContent).forEach(char => {
 // End of About Me Text
 
 //Projects
+const container = document.querySelector('.container')
 const projects = document.querySelectorAll('.project') 
+const projectHideBtn = document.querySelector('.project-hide-btn')
 
 projects.forEach(project =>{
  project.addEventListener('mouseenter', () => {
      project.firstElementChild.style.top = `-${project.firstElementChild.offsetHeight - project.offsetHeight + 20}px`;
  });
 
- project.addEventListener('mouseleave', () =>{
+ project.addEventListener('mouseleave', () => {
      project.firstElementChild.style.top = '2rem';
  })
 
-})
+//Big Project Image
+project.addEventListener('click', () => {
+    const bigImgWrapper = document.createElement('div')
+    bigImgWrapper.className = 'project-img-wrapper';
+    container.appendChild(bigImgWrapper)
+
+ const bigImg = document.createElement('img')
+  bigImg.className = 'project-img'
+  const imgPath = project.firstElementChild.getAttribute('src').split('.')[0];
+  bigImg.setAttribute('src',`${imgPath}-big.jpg`);
+  bigImgWrapper.appendChild(bigImg);
+  document.body.style.overflowY = 'hidden';
+
+  projectHideBtn.classList.add('change')
+
+  projectHideBtn.onclick = () => {
+      projectHideBtn.classList.remove('change');
+      bigImgWrapper.remove()
+      document.body.style.overflowY = 'scroll'
+  }
+});
+//End of Big Project Image
+});
 //End of Projects 
