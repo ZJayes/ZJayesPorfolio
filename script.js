854c4aae3aeff32630ddc578c835bde25c7e76fd
Zachary Jayes <zacjayes@gmail.com>
2021-09-24 13:22:31 -0500

    created and customized services

--- script.js
+++ script.js
@@ -183,3 +183,18 @@ projectsBtn.addEventListener('click', (e) => {
 });
 //End of Projects Button
 //End of Projects 
+
+//Section 4
+document.querySelectorAll('.service-btn').forEach((service) => {
+    service.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        const serviceText = service.nextElementSibling;
+        serviceText.classList.toggle('change');
+
+        const rightPosition = serviceText.classList.contains('change') ? `calc(100% - ${getComputedStyle(service.firstElementChild).width})` : 0;
+
+        service.firstElementChild.style.right = rightPosition;
+    })
+})
+//End of Section 4
