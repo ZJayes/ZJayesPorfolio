26285606b93a0d9b9e50244ae9a84d24ebb9f1ac
Zachary Jayes <zacjayes@gmail.com>
2021-10-03 19:40:18 -0500

    creae and customize contact form

--- script.js
+++ script.js
@@ -198,3 +198,30 @@ document.querySelectorAll('.service-btn').forEach((service) => {
     })
 })
 //End of Section 4
+
+//Section 5
+//Form
+const formHeading = document.querySelector('.form-heading');
+const formInputs = document.querySelectorAll('.contact-form-input');
+
+formInputs.forEach((input) => {
+    input.addEventListener('focus', () => {
+        formHeading.style.opacity = "0"
+        setTimeout(() => {
+            formHeading.textContent = `Your ${input.placeholder}`;
+            formHeading.style.opacity = "1"
+        }, 300);
+
+    });
+    input.addEventListener('blur', () => {
+        formHeading.style.opacity = "0"
+        setTimeout(() => {
+            formHeading.textContent = "Let's Talk";
+            formHeading.style.opacity = "1"
+        }, 300);
+    });
+});
+
+//End of Form
+
+//End of Section 5
