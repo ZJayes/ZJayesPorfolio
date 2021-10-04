c41f7986f46a56dc0e54eb73a2cf4a87719e760c
Zachary Jayes <zacjayes@gmail.com>
2021-10-03 21:03:00 -0500

    create and customize social media slideshow

--- script.js
+++ script.js
@@ -224,4 +224,30 @@ formInputs.forEach((input) => {
 
 //End of Form
 
+
+// Slideshow
+const slideshow = document.querySelector('.slideshow');
+
+setInterval(() => {
+    const firstIcon = slideshow.firstElementChild;
+
+    firstIcon.classList.add('faded-out');
+
+    const thirdIcon = slideshow.children[3]
+
+    thirdIcon.classList.add('light');
+
+    thirdIcon.previousElementSibling.classList.remove('light')
+
+    setTimeout(() => {
+        slideshow.removeChild(firstIcon);
+
+        slideshow.appendChild(firstIcon);
+
+        setTimeout(() => {
+            firstIcon.classList.remove('faded-out');
+        }, 500);
+    }, 500);
+}, 3000);
+//End of Slideshow
 //End of Section 5
